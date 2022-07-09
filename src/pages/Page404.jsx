d19ac2a4759e7722bf834215cf9c1cbde187ed2a
Dmytro <kotykhin_d@ukr.net>
2022-07-09 13:25:12 +0300

    error page upgrade

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,17 +1,7 @@
-import ErrorMessage from "../components/errorMessage/ErrorMessage";
-import { Link } from "react-router-dom";
-import { Typography } from "@mui/material";
-
-import "./page404.scss";
+import Error from "../components/error/Error";
 
 const Page404 = () => {
-    return (
-        <div>
-            <ErrorMessage />
-            <Typography className="page">Сторінка не знайдена</Typography>
-            <Link className="link" to="/">Повернутися на головну</Link>
-        </div>
-    );
+    return <Error />;
 };
 
 export default Page404;
